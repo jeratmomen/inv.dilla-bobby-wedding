42e949e3cade2d528d6666aa1eb07d1ed3e61368
jeratmomen <jeratmomen@gmail.com>
2026-09-08 09:24:00 +0700

    Add files via upload

--- app/invitation-assets.ts
+++ app/invitation-assets.ts
@@ -2,5 +2,7 @@ export const invitationAssets = {
   bride: "/photos/dilla.png",
   groom: "/photos/bobby.png",
   couple: "/photos/dilla-bobby.png",
+  brideGroom: "/photos/dilla-bobby.png",
+  bigDay: ["/photos/dilla.png", "/photos/dilla-bobby.png", "/photos/bobby.png"],
   music: "/music/muara-adera-trimmed.mp3"
 };
