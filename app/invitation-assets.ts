c51f8feb1e70c153582a89568ea8efcad11d32dd
jeratmomen <jeratmomen@gmail.com>
2026-09-08 09:42:48 +0700

    Update invitation-assets.ts

--- app/invitation-assets.ts
+++ app/invitation-assets.ts
@@ -2,7 +2,7 @@ export const invitationAssets = {
   bride: "/photos/dilla.png",
   groom: "/photos/bobby.png",
   couple: "/photos/dilla-bobby.png",
-  brideGroom: "/photos/dilla-bobby.png",
-  bigDay: ["/photos/dilla.png", "/photos/dilla-bobby.png", "/photos/bobby.png"],
+  brideGroom: "/photos/bride & groom.png",
+  bigDay: ["/photos/strip 1.jpg", "/photos/strip 2.PNG", "/photos/strip 3.png"],
   music: "/music/muara-adera-trimmed.mp3"
 };
